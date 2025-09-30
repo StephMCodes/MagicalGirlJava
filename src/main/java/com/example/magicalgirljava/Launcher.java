c531ba2d31992dfa07c9bd3b5e02f8b451bafab1
Bella P. <bellaperezppg@gmail.com>
2025-09-30 12:53:47 -0400

    test comment

--- src/main/java/com/example/magicalgirljava/Launcher.java
+++ src/main/java/com/example/magicalgirljava/Launcher.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 public class Launcher {
     //comment test
     //comment test ruth
+    //comment test bella ;-;
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
