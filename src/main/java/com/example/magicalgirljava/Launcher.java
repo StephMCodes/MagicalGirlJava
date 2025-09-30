52ef4e0510389be45a51840e1d21948030935cea
RuthieGG <ruthggarcia99@gmail.com>
2025-09-30 12:38:52 -0400

    Comment test 2

--- src/main/java/com/example/magicalgirljava/Launcher.java
+++ src/main/java/com/example/magicalgirljava/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     //comment test
+    //comment test ruth
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
     }
